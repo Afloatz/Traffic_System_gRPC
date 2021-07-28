19010ccd54582522f922bf080ec46e11d13532f2
Afloatz <florentlaot.fr@gmail.com>
2021-07-28 17:33:46 +0100

    built channel

--- TrafficSystemGrpc/src/main/java/grpc/trafficservice/TrafficClient.java
+++ TrafficSystemGrpc/src/main/java/grpc/trafficservice/TrafficClient.java
@@ -1,5 +1,19 @@
 package grpc.trafficservice;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class TrafficClient {
+	
+	public static void main(String[] args) {
+		
+		//Build a channel (to connect the server and client)
+		int port = 50051; 
+		String host = "localhost"; // location of the server
+		
+		//Create the channel 
+		ManagedChannel newChannel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+	}
+	
 
 }
