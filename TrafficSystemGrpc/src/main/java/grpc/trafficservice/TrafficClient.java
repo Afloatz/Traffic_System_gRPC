350db125bc14450ef6c671c9ec73c500bcb37796
Afloatz <florentlaot.fr@gmail.com>
2021-07-29 16:08:24 +0100

    client request and exception handling

--- TrafficSystemGrpc/src/main/java/grpc/trafficservice/TrafficClient.java
+++ TrafficSystemGrpc/src/main/java/grpc/trafficservice/TrafficClient.java
@@ -1,12 +1,14 @@
 package grpc.trafficservice;
 
+import java.util.concurrent.TimeUnit;
+
 import grpc.trafficservice.trafficServiceGrpc.trafficServiceBlockingStub;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
 public class TrafficClient {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		//Build a channel (to connect the server and client)
 		int port = 50051; 
@@ -17,12 +19,20 @@ public class TrafficClient {
 	
 		//now build our message
 		
-		RequestEmergency rString = RequestEmergency.newBuilder().setMessage("Emergency at Capel Street").build();
+		RequestEmergency rEmergency = RequestEmergency.newBuilder().setMessage("Emergency at Capel Street").build();
 	
 		//Create a stub, pass the channel to the stub
 		//for unary -> need a BlockingStub
 		trafficServiceBlockingStub bStub = trafficServiceGrpc.newBlockingStub(trafficChannel);
+	
+		// bring back the message from the server
+		EmergencyResponse response = bStub.sendEmergency(rEmergency);
+		System.out.println(response.getText());
+		
+		trafficChannel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+	
 	}
 	
 
 }
+;
